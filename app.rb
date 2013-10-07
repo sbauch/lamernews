a7f1b3dfcb6508159fc6729f1270f5ee6bee7540
Sam Bauch <sbauch@gmail.com>
2013-10-07 12:03:17 -0400

    redis setup

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ Version = "0.11.0"
 
 def setup_redis(uri=RedisURL)
     uri = URI.parse(uri)
-    $r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) unless $r
+    $r = Redis.new(:host => uri.host, :port => uri.port, :password => '8DPofDKbDbIo9GEu') unless $r
 end
 
 before do
